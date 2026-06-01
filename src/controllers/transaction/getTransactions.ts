948fdf703e6952871a30d90f0da01c80b13c3b5b
samuelkime267 <samuelkime7@gmail.com>
2026-06-01 14:03:50 +0100

    working on pagination

--- src/controllers/transaction/getTransactions.ts
+++ src/controllers/transaction/getTransactions.ts
@@ -3,6 +3,13 @@ import { UserDocument } from "@/models/user.model";
 import { CustomError } from "@/types";
 import { NextFunction, Request, Response } from "express";
 
+/**
+ * I want to include pagination and filter
+ *  for pagination - current page, count, remaining page
+ * for filter - type="income or expense", date- time period start time and stop time,
+ *
+ */
+
 export const getTransactions = async (
   req: Request,
   res: Response,
@@ -10,22 +17,32 @@ export const getTransactions = async (
 ) => {
   try {
     const user = req.user as UserDocument;
-    const { id } = req.params;
-
-    // if (!id || typeof id !== "string") {
-    //   const error = new Error("Invalid id") as CustomError;
-    //   error.statusCode = 400;
-    //   throw error;
-    // }
+    const currentPage = Number(req.query.page) || 1;
+    const itemCount = Number(req.query.count) || 20;
+    const type = req.query.type;
+    const startDate = Number(req.query.startDate) || 0;
+    const endDate = Number(req.query.endDate) || Date.now();
 
     const transactions = await Transaction.find({
       user: user._id,
-    });
+      ...(type && { type }),
+      date: { $gte: startDate, $lte: endDate },
+    })
+      .limit(itemCount)
+      .skip((currentPage - 1) * itemCount);
 
     res.status(200).json({
       message: "Transactions retrieved successfully",
       success: true,
       data: transactions,
+      // continue with this
+      pagination: {
+        currentPage,
+        nextPage: 0,
+        maxPage: 0,
+        count: itemCount,
+        totalItems: 0,
+      },
     });
   } catch (error) {
     next(error);
