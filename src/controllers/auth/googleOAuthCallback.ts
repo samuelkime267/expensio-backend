16136e4b81b4711dfc85ca948f41bbc596995e5f
samuelkime267 <samuelkime7@gmail.com>
2026-07-04 15:56:27 +0100

    path changed

--- src/controllers/auth/googleOAuthCallback.ts
+++ src/controllers/auth/googleOAuthCallback.ts
@@ -44,7 +44,8 @@ export const googleOAuthCallback = async (
         secure: IS_PROD,
         sameSite: IS_PROD ? "none" : "lax",
         maxAge: 30 * 24 * 60 * 60 * 1000,
-        path: "/api/v1/auth",
+        path: "/",
+        // path: "/api/v1/auth",
       }).send(`
         <html>
           <body>
