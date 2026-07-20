078f99d9362695264290716612457d8813f12239
samuelkime267 <samuelkime7@gmail.com>
2026-07-20 01:00:12 +0100

    updated delete transaction funtionality to updated user amount after deleting transaction

--- src/controllers/transaction/deleteTransaction.ts
+++ src/controllers/transaction/deleteTransaction.ts
@@ -2,12 +2,14 @@ import Transaction from "@/models/transaction.model";
 import { UserDocument } from "@/models/user.model";
 import { CustomError } from "@/types";
 import { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
 
 export const deleteTransaction = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
+  const session = await mongoose.startSession();
   try {
     const user = req.user as UserDocument;
     const { id } = req.params;
@@ -17,15 +19,28 @@ export const deleteTransaction = async (
       error.statusCode = 400;
       throw error;
     }
-    const deletedIncome = await Transaction.findOneAndDelete({
-      _id: id,
-      user: user._id,
-    });
-    if (!deletedIncome) {
+    const deletedTransaction = await Transaction.findOneAndDelete(
+      {
+        _id: id,
+        user: user._id,
+      },
+      {
+        session,
+      },
+    );
+    if (!deletedTransaction) {
       const error = new Error("Transaction not found") as CustomError;
       error.statusCode = 404;
       throw error;
     }
+
+    if (deletedTransaction.type === "Income") {
+      user.balance -= deletedTransaction.amount;
+    }
+    if (deletedTransaction.type === "Expense") {
+      user.balance += deletedTransaction.amount;
+    }
+    await user.save({ session });
 
     res.status(200).json({
       message: "Transaction deleted successfully",
